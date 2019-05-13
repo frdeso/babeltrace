3c33429517c887de21718e8c3641ed15edc92bec
Michael Jeanson <mjeanson@efficios.com>
2019-05-13 10:32:23 -0400

    WIP test_bin_info
    Change-Id: Idbf999972ea11b72356f4b44f295d9cb485f588a

--- tests/plugins/test_bin_info.c
+++ tests/plugins/test_bin_info.c
@@ -23,7 +23,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <glib.h>
 
+#include <babeltrace/babeltrace-internal.h>
 #include <babeltrace/assert-internal.h>
 #include <lttng-utils/debug-info/bin-info.h>
 
@@ -31,26 +33,29 @@
 
 #define NR_TESTS 36
 #define SO_NAME "libhello_so"
-#define SO_NAME_ELF "libhello_elf_so"
-#define SO_NAME_BUILD_ID "libhello_build_id_so"
-#define SO_NAME_DEBUG_LINK "libhello_debug_link_so"
 
-#define SO_LOW_ADDR 0x400000
-#define SO_MEMSZ 0x400000
+#define DWARF_DIR_NAME "dwarf_full"
+#define ELF_DIR_NAME "elf_only"
+#define BUILDID_DIR_NAME "build_id"
+#define DEBUGLINK_DIR_NAME "debug_link"
 
-#define FUNC_FOO_ADDR 0x4014ee
-#define FUNC_FOO_LINE_NO 8
-#define FUNC_FOO_FILENAME "/efficios/libhello.c"
+#define SO_INV_ADDR 0x200000 /* Out of bound address */
+#define SO_LOW_ADDR 0x400000 /* Lower bound of address mapping */
+#define SO_MEMSZ 0x800000 /* Size of address mapping */
 
-#define FUNC_FOO_TP_ADDR 0x4014d3
-#define FUNC_FOO_TP_LINE_NO 7
-#define FUNC_FOO_TP_FILENAME "/efficios/libhello.c"
+#define FUNC_FOO_ADDR 0x2277
+#define FUNC_FOO_FILENAME "libhello.c"
 
-#define FUNC_FOO_ADDR_ELF 0x4013ef
-#define FUNC_FOO_ADDR_DBG_LINK 0x40148e
+/* printf statement in foo() */
+#define FUNC_FOO_PRINTF_OFFSET 0xf0
+#define FUNC_FOO_PRINTF_ADDR (SO_LOW_ADDR + FUNC_FOO_ADDR + FUNC_FOO_PRINTF_OFFSET)
+#define FUNC_FOO_PRINTF_LINE_NO 36
+#define FUNC_FOO_PRINTF_NAME "foo+" TOSTRING(FUNC_FOO_PRINTF_OFFSET)
 
-#define FUNC_FOO_NAME "foo+0xc3"
-#define FUNC_FOO_NAME_ELF "foo+0x24"
+/* inlined tracepoint in foo() */
+#define FUNC_FOO_TP_OFFSET 0x89
+#define FUNC_FOO_TP_ADDR (SO_LOW_ADDR + FUNC_FOO_ADDR + FUNC_FOO_TP_OFFSET)
+#define FUNC_FOO_TP_LINE_NO 35
 
 #define BUILD_ID_LEN 20
 
@@ -58,10 +63,10 @@ char *opt_debug_info_dir;
 char *opt_debug_info_target_prefix;
 
 static
-void test_bin_info_build_id(const char *data_dir)
+void test_bin_info_build_id(const char *bin_info_dir)
 {
 	int ret;
-	char path[PATH_MAX];
+	char *data_dir, *bin_path;
 	char *func_name = NULL;
 	struct bin_info *bin = NULL;
 	struct source_location *src_loc = NULL;
@@ -73,34 +78,40 @@ void test_bin_info_build_id(const char *data_dir)
 
 	diag("bin-info tests - separate DWARF via build ID");
 
-	snprintf(path, PATH_MAX, "%s/%s", data_dir, SO_NAME_BUILD_ID);
+	data_dir = g_build_filename(bin_info_dir, BUILDID_DIR_NAME, NULL);
+	bin_path = g_build_filename(bin_info_dir, BUILDID_DIR_NAME, SO_NAME, NULL);
+
+	if (data_dir == NULL || bin_path == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
 	ret = bt_fd_cache_init(&fdc);
 	BT_ASSERT(ret == 0);
-	bin = bin_info_create(&fdc, path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
-	ok(bin != NULL, "bin_info_create successful");
+
+	bin = bin_info_create(&fdc, bin_path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
+	ok(bin != NULL, "bin_info_create successful (%s)", bin_path);
 
 	/* Test setting build_id */
 	ret = bin_info_set_build_id(bin, build_id, BUILD_ID_LEN);
 	ok(ret == 0, "bin_info_set_build_id successful");
 
 	/* Test function name lookup (with DWARF) */
-	ret = bin_info_lookup_function_name(bin, FUNC_FOO_ADDR, &func_name);
-	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_ADDR);
+	ret = bin_info_lookup_function_name(bin, FUNC_FOO_PRINTF_ADDR, &func_name);
+	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (func_name) {
-		ok(strcmp(func_name, FUNC_FOO_NAME) == 0,
-			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_NAME, func_name);
+		ok(strcmp(func_name, FUNC_FOO_PRINTF_NAME) == 0,
+			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_PRINTF_NAME, func_name);
 		free(func_name);
 	} else {
-		skip(1, "bin_info_lookup_function_name - func_name is NULL");
+		fail("bin_info_lookup_function_name - func_name is NULL");
 	}
 
 	/* Test source location lookup */
-	ret = bin_info_lookup_source_location(bin, FUNC_FOO_ADDR, &src_loc);
-	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_ADDR);
+	ret = bin_info_lookup_source_location(bin, FUNC_FOO_PRINTF_ADDR, &src_loc);
+	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (src_loc) {
-		ok(src_loc->line_no == FUNC_FOO_LINE_NO,
-			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_LINE_NO, src_loc->line_no);
+		ok(src_loc->line_no == FUNC_FOO_PRINTF_LINE_NO,
+			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_PRINTF_LINE_NO, src_loc->line_no);
 		ok(strcmp(src_loc->filename, FUNC_FOO_FILENAME) == 0,
 			"bin_info_lookup_source_location - correct filename (%s == %s)", FUNC_FOO_FILENAME, src_loc->filename);
 		source_location_destroy(src_loc);
@@ -110,13 +121,15 @@ void test_bin_info_build_id(const char *data_dir)
 
 	bin_info_destroy(bin);
 	bt_fd_cache_fini(&fdc);
+	g_free(data_dir);
+	g_free(bin_path);
 }
 
 static
-void test_bin_info_debug_link(const char *data_dir)
+void test_bin_info_debug_link(const char *bin_info_dir)
 {
 	int ret;
-	char path[PATH_MAX];
+	char *data_dir, *bin_path;
 	char *func_name = NULL;
 	struct bin_info *bin = NULL;
 	struct source_location *src_loc = NULL;
@@ -126,37 +139,43 @@ void test_bin_info_debug_link(const char *data_dir)
 
 	diag("bin-info tests - separate DWARF via debug link");
 
-	snprintf(path, PATH_MAX, "%s/%s", data_dir, SO_NAME_DEBUG_LINK);
+	data_dir = g_build_filename(bin_info_dir, DEBUGLINK_DIR_NAME, NULL);
+	bin_path = g_build_filename(bin_info_dir, DEBUGLINK_DIR_NAME, SO_NAME, NULL);
+
+	if (data_dir == NULL || bin_path == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
 	ret = bt_fd_cache_init(&fdc);
 	BT_ASSERT(ret == 0);
-	bin = bin_info_create(&fdc, path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir,
+
+	bin = bin_info_create(&fdc, bin_path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir,
 			NULL);
-	ok(bin != NULL, "bin_info_create successful");
+	ok(bin != NULL, "bin_info_create successful (%s)", bin_path);
 
 	/* Test setting debug link */
 	ret = bin_info_set_debug_link(bin, dbg_filename, crc);
 	ok(ret == 0, "bin_info_set_debug_link successful");
 
 	/* Test function name lookup (with DWARF) */
-	ret = bin_info_lookup_function_name(bin, FUNC_FOO_ADDR_DBG_LINK,
+	ret = bin_info_lookup_function_name(bin, FUNC_FOO_PRINTF_ADDR,
 					&func_name);
-	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_ADDR_DBG_LINK);
+	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (func_name) {
-		ok(strcmp(func_name, FUNC_FOO_NAME) == 0,
-			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_NAME, func_name);
+		ok(strcmp(func_name, FUNC_FOO_PRINTF_NAME) == 0,
+			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_PRINTF_NAME, func_name);
 		free(func_name);
 	} else {
 		skip(1, "bin_info_lookup_function_name - func_name is NULL");
 	}
 
 	/* Test source location lookup */
-	ret = bin_info_lookup_source_location(bin, FUNC_FOO_ADDR_DBG_LINK,
+	ret = bin_info_lookup_source_location(bin, FUNC_FOO_PRINTF_ADDR,
 					&src_loc);
-	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_ADDR_DBG_LINK);
+	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (src_loc) {
-		ok(src_loc->line_no == FUNC_FOO_LINE_NO,
-			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_LINE_NO, src_loc->line_no);
+		ok(src_loc->line_no == FUNC_FOO_PRINTF_LINE_NO,
+			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_PRINTF_LINE_NO, src_loc->line_no);
 		ok(strcmp(src_loc->filename, FUNC_FOO_FILENAME) == 0,
 			"bin_info_lookup_source_location - correct filename (%s == %s)", FUNC_FOO_FILENAME, src_loc->filename);
 		source_location_destroy(src_loc);
@@ -166,13 +185,15 @@ void test_bin_info_debug_link(const char *data_dir)
 
 	bin_info_destroy(bin);
 	bt_fd_cache_fini(&fdc);
+	g_free(data_dir);
+	g_free(bin_path);
 }
 
 static
-void test_bin_info_elf(const char *data_dir)
+void test_bin_info_elf(const char *bin_info_dir)
 {
 	int ret;
-	char path[PATH_MAX];
+	char *data_dir, *bin_path;
 	char *func_name = NULL;
 	struct bin_info *bin = NULL;
 	struct source_location *src_loc = NULL;
@@ -180,19 +201,25 @@ void test_bin_info_elf(const char *data_dir)
 
 	diag("bin-info tests - ELF only");
 
-	snprintf(path, PATH_MAX, "%s/%s", data_dir, SO_NAME_ELF);
+	data_dir = g_build_filename(bin_info_dir, ELF_DIR_NAME, NULL);
+	bin_path = g_build_filename(bin_info_dir, ELF_DIR_NAME, SO_NAME, NULL);
+
+	if (data_dir == NULL || bin_path == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
 	ret = bt_fd_cache_init(&fdc);
 	BT_ASSERT(ret == 0);
-	bin = bin_info_create(&fdc, path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
-	ok(bin != NULL, "bin_info_create successful");
+
+	bin = bin_info_create(&fdc, bin_path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
+	ok(bin != NULL, "bin_info_create successful (%s)", bin_path);
 
 	/* Test function name lookup (with ELF) */
-	ret = bin_info_lookup_function_name(bin, FUNC_FOO_ADDR_ELF, &func_name);
+	ret = bin_info_lookup_function_name(bin, FUNC_FOO_PRINTF_ADDR, &func_name);
 	ok(ret == 0, "bin_info_lookup_function_name successful");
 	if (func_name) {
-		ok(strcmp(func_name, FUNC_FOO_NAME_ELF) == 0,
-			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_NAME_ELF, func_name);
+		ok(strcmp(func_name, FUNC_FOO_PRINTF_NAME) == 0,
+			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_PRINTF_NAME, func_name);
 		free(func_name);
 		func_name = NULL;
 	} else {
@@ -205,75 +232,87 @@ void test_bin_info_elf(const char *data_dir)
 		"bin_info_lookup_function_name - fail on addr not found");
 
 	/* Test source location location - should fail on ELF only file  */
-	ret = bin_info_lookup_source_location(bin, FUNC_FOO_ADDR_ELF, &src_loc);
+	ret = bin_info_lookup_source_location(bin, FUNC_FOO_PRINTF_ADDR, &src_loc);
 	ok(ret == -1, "bin_info_lookup_source_location - fail on ELF only file");
 
 	source_location_destroy(src_loc);
 	bin_info_destroy(bin);
 	bt_fd_cache_fini(&fdc);
+	g_free(data_dir);
+	g_free(bin_path);
 }
 
 static
-void test_bin_info(const char *data_dir)
+void test_bin_info(const char *bin_info_dir)
 {
 	int ret;
-	char path[PATH_MAX];
+	char *data_dir, *bin_path;
 	char *func_name = NULL;
 	struct bin_info *bin = NULL;
 	struct source_location *src_loc = NULL;
 	struct bt_fd_cache fdc;
 
-	diag("bin-info tests - DWARF bundled with SO file");
+	diag("bin-info tests - DWARF bundled in SO file");
 
+	data_dir = g_build_filename(bin_info_dir, DWARF_DIR_NAME, NULL);
+	bin_path = g_build_filename(bin_info_dir, DWARF_DIR_NAME, SO_NAME, NULL);
 
-	snprintf(path, PATH_MAX, "%s/%s", data_dir, SO_NAME);
+	if (data_dir == NULL || bin_path == NULL) {
+		exit(EXIT_FAILURE);
+	}
 
 	ret = bt_fd_cache_init(&fdc);
 	BT_ASSERT(ret == 0);
-	bin = bin_info_create(&fdc, path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
-	ok(bin != NULL, "bin_info_create successful");
+
+	bin = bin_info_create(&fdc, bin_path, SO_LOW_ADDR, SO_MEMSZ, true, data_dir, NULL);
+	ok(bin != NULL, "bin_info_create successful (%s)", bin_path);
 
 	/* Test bin_info_has_address */
-	ret = bin_info_has_address(bin, 0);
+	ret = bin_info_has_address(bin, SO_LOW_ADDR - 1);
 	ok(ret == 0, "bin_info_has_address - address under so's range");
+
 	ret = bin_info_has_address(bin, SO_LOW_ADDR);
 	ok(ret == 1, "bin_info_has_address - lower bound of so's range");
-	ret = bin_info_has_address(bin, FUNC_FOO_ADDR);
+
+	ret = bin_info_has_address(bin, FUNC_FOO_PRINTF_ADDR);
 	ok(ret == 1, "bin_info_has_address - address in so's range");
+
 	ret = bin_info_has_address(bin, SO_LOW_ADDR + SO_MEMSZ - 1);
 	ok(ret == 1, "bin_info_has_address - upper bound of so's range");
+
 	ret = bin_info_has_address(bin, SO_LOW_ADDR + SO_MEMSZ);
 	ok(ret == 0, "bin_info_has_address - address above so's range");
 
 	/* Test function name lookup (with DWARF) */
-	ret = bin_info_lookup_function_name(bin, FUNC_FOO_ADDR, &func_name);
-	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_ADDR);
+	ret = bin_info_lookup_function_name(bin, FUNC_FOO_PRINTF_ADDR, &func_name);
+	ok(ret == 0, "bin_info_lookup_function_name successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (func_name) {
-		ok(strcmp(func_name, FUNC_FOO_NAME) == 0,
-			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_NAME, func_name);
+		ok(strcmp(func_name, FUNC_FOO_PRINTF_NAME) == 0,
+			"bin_info_lookup_function_name - correct func_name (%s == %s)", FUNC_FOO_PRINTF_NAME, func_name);
 		free(func_name);
 		func_name = NULL;
 	} else {
-		skip(1, "bin_info_lookup_function_name - func_name is NULL");
+		fail("bin_info_lookup_function_name - func_name is NULL");
 	}
 
 	/* Test function name lookup - erroneous address */
-	ret = bin_info_lookup_function_name(bin, 0, &func_name);
+	ret = bin_info_lookup_function_name(bin, SO_INV_ADDR, &func_name);
 	ok(ret == -1 && func_name == NULL,
 		"bin_info_lookup_function_name - fail on addr not found");
 
 	/* Test source location lookup */
-	ret = bin_info_lookup_source_location(bin, FUNC_FOO_ADDR, &src_loc);
-	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_ADDR);
+	ret = bin_info_lookup_source_location(bin, FUNC_FOO_PRINTF_ADDR, &src_loc);
+	ok(ret == 0, "bin_info_lookup_source_location successful at 0x%x", FUNC_FOO_PRINTF_ADDR);
 	if (src_loc) {
-		ok(src_loc->line_no == FUNC_FOO_LINE_NO,
-			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_LINE_NO, src_loc->line_no);
+		ok(src_loc->line_no == FUNC_FOO_PRINTF_LINE_NO,
+			"bin_info_lookup_source_location - correct line_no (%d == %d)", FUNC_FOO_PRINTF_LINE_NO, src_loc->line_no);
 		ok(strcmp(src_loc->filename, FUNC_FOO_FILENAME) == 0,
 			"bin_info_lookup_source_location - correct filename (%s == %s)", FUNC_FOO_FILENAME, src_loc->filename);
 		source_location_destroy(src_loc);
 		src_loc = NULL;
 	} else {
-		skip(2, "bin_info_lookup_source_location - src_loc is NULL");
+		fail("bin_info_lookup_source_location - src_loc is NULL");
+		fail("bin_info_lookup_source_location - src_loc is NULL");
 	}
 
 	/* Test source location lookup - inlined function */
@@ -283,21 +322,24 @@ void test_bin_info(const char *data_dir)
 	if (src_loc) {
 		ok(src_loc->line_no == FUNC_FOO_TP_LINE_NO,
 			"bin_info_lookup_source_location (inlined func) - correct line_no (%d == %d)", FUNC_FOO_TP_LINE_NO, src_loc->line_no);
-		ok(strcmp(src_loc->filename, FUNC_FOO_TP_FILENAME) == 0,
-			"bin_info_lookup_source_location (inlined func) - correct filename (%s == %s)", FUNC_FOO_TP_FILENAME, src_loc->filename);
+		ok(strcmp(src_loc->filename, FUNC_FOO_FILENAME) == 0,
+			"bin_info_lookup_source_location (inlined func) - correct filename (%s == %s)", FUNC_FOO_FILENAME, src_loc->filename);
 		source_location_destroy(src_loc);
 		src_loc = NULL;
 	} else {
-		skip(2, "bin_info_lookup_source_location (inlined func) - src_loc is NULL");
+		fail("bin_info_lookup_source_location (inlined func) - src_loc is NULL");
+		fail("bin_info_lookup_source_location (inlined func) - src_loc is NULL");
 	}
 
 	/* Test source location lookup - erroneous address */
-	ret = bin_info_lookup_source_location(bin, 0, &src_loc);
+	ret = bin_info_lookup_source_location(bin, SO_INV_ADDR, &src_loc);
 	ok(ret == -1 && src_loc == NULL,
 		"bin_info_lookup_source_location - fail on addr not found");
 
 	bin_info_destroy(bin);
 	bt_fd_cache_fini(&fdc);
+	g_free(data_dir);
+	g_free(bin_path);
 }
 
 int main(int argc, char **argv)
